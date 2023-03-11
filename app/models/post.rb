ba81b95dcbdf3ba1618d7abf1cc36f22f7c834b4
bornittah <bornittah@gmail.com>
2023-03-11 14:20:39 +0300

    Add required changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: :post_id
   after_save :update_posts_counter
 
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
   def update_posts_counter
     author.increment!(:posts_counter)
   end
