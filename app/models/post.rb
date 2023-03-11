2797ca9d7839cbbb301eecff00c5f00b69abcfec
bornittah <bornittah@gmail.com>
2023-03-11 13:46:04 +0300

    Fix linters errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
   after_save :update_posts_counter
-  
+
   def update_posts_counter
     author.increment!(:posts_counter)
   end
