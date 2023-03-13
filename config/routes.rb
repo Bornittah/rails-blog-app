52cc4b58e65dc77efcfc60ed5bc2e54446b76ada
bornittah <bornittah@gmail.com>
2023-03-13 18:28:52 +0300

    Create controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "users#index"
+  resources :users, only: [:index, :show]  do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
+  end
 end
