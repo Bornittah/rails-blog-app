f1933f095d74e669d47c72e9894648ea938c1fe3
bornittah <bornittah@gmail.com>
2023-03-15 18:05:49 +0300

    Add unit tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  before(:each) do
+    @user = User.create(name: 'Agasha', photo: '', bio: 'Software developer', posts_counter: 0)
+    @post = Post.create(
+      title: 'Post title',
+      text: 'Post text',
+      comments_counter: 0,
+      likes_counter: 0
+    )
+    Comment.create(text: 'Happy coding')
+    Comment.create(text: 'Good job team')
+    Comment.create(text: 'Status approved!')
+  end
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:each) do
+      get user_posts_path(@user)
+    end
+
+    it 'return correct response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'body should includes correct placeholder text' do
+      expect(response.body).to include('my posts')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:each) do
+      get user_posts_path(@user, @post)
+    end
+
+    it 'return correct response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to_not render_template(:show)
+    end
+
+    it 'body should includes correct placeholder text' do
+      expect(response.body).to include('posts')
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe UsersController, type: :request do
+  describe ':index' do
+    before(:each) { get root_path }
+
+    it('status should be :ok') do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it('rendered :index template') do
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe ':show' do
+    before(:each) do
+      @author = User.create(name: 'Agasha', posts_counter: 0, photo: '', bio: '')
+      get user_path(@author)
+    end
+
+    it('status should be :ok') do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it('should render :show template') do
+      expect(response).to render_template(:show)
+    end
   end
 end
