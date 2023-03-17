576348f9621fe82aa80ae0b66cdd410e7bd8e73f
bornittah <bornittah@gmail.com>
2023-03-17 19:28:37 +0300

    Create view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id].to_i)
+    @post = @user.posts
+  end
 
-  def show; end
+  def show
+    @post = Post.joins(:user).where(user: { id: params[:user_id] }).find(params[:id])
+    @comments = @post.comments
+  end
 end
