3b22f9bc767b9f48b6067e0fed0e4c175f9e9a17
bornittah <bornittah@gmail.com>
2023-03-18 23:22:44 +0300

    Create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,7 @@
 class PostsController < ApplicationController
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to user_posts_path(params[:user_id])
-    else
-      render :new
-    end
-  end
-
   def index
     @user = User.find(params[:user_id].to_i)
-    @posts = Post.where(author_id: params[:user_id]) || 'No posts yet'
+    @posts = Post.where(author_id: params[:user_id])
   end
 
   def show
@@ -22,9 +9,34 @@ class PostsController < ApplicationController
     @comments = @post.comments
   end
 
-  private
+  def new
+    @post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: @post } }
+    end
+  end
 
   def post_params
-    params.require(:post).permit(:title, :text).merge(author_id: current_user.id)
+    params
+      .require(:post)
+      .permit(:title, :text)
+      .merge(author: current_user, comments_counter: 0, likes_counter: 0)
   end
+
+  def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+      format.html do
+        if @post.save
+          flash[:success] = 'Post saved successfully'
+          redirect_to user_post_path(current_user, @post)
+        else
+          flash.now[:error] = 'Error: Post could not be saved'
+          redirect_to new_user_post_path(current_user)
+        end
+      end
+    end
+  end
+
+  private :post_params
 end
