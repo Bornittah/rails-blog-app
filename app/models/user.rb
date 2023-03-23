beb32e7ec9e99e4198804d8c7878bc763da94586
pius ssekweyama <ssekweyamapius@gmail.com>
2023-03-23 16:49:59 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :posts, foreign_key: :author_id
-  has_many :comments, foreign_key: :author_id
-  has_many :likes, foreign_key: :author_id
+
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+  # has_many :posts, foreign_key: :author_id
+  # has_many :comments, foreign_key: :author_id
+  # has_many :likes, foreign_key: :author_id
 
   validates :name, presence: true, length: { minimum: 2, maximum: 30 }
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
