1e129cfa6d4f4f7c669b30b0e267d10a49efaa36
pius ssekweyama <ssekweyamapius@gmail.com>
2023-03-23 15:42:32 +0300

    intergration tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,55 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  describe 'Tests for Post model validation ' do
-    User.create(name: 'Tiina', photo: 'https://unsplash.com/photos/6PP8DrhY0aA', bio: 'Coding girl')
-
-    subject { Post.new(author: user, title: 'Project', text: 'DESC') }
-    before { subject.save }
-
-    it 'title should be present' do
-      subject.title = nil
-      expect(subject).to_not be_valid
-    end
-
-    it 'title should not be blank' do
-      subject.title = ''
-      expect(subject).to_not be_valid
-    end
-
-    it 'title should not exceed 250 characters' do
-      subject.title = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
-             took a galley of type and scrambled it to make a type specimen book.
-              It has survived not only five centuries"
-      expect(subject).to_not be_valid
-    end
-
-    it 'text should be less than 500 characters' do
-      subject.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
-            took a galley of type and scrambled it to make a type specimen book.
-            It has survived not only five centuries"
-      expect(subject).to_not be_valid
-    end
-
-    it 'comments counter should be greater than or equal than 0' do
-      subject.comments_conter = -3
-      expect(subject).to_not be_valid
-    end
-
-    it 'likes counter should be greater than or equal than 0' do
-      subject.likes_counter = -3
-      expect(subject).to_not be_valid
-    end
+  author1 = User.create(name: 'paul', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'bio 1')
+  subject do
+    Post.new(title: 'titl 1', body: 'bio 1', author: author1, comments_counter: 1,
+             likes_counter: 2)
   end
 
-  describe 'Tests for Post model methods' do
-    test_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-    before { 10.times { Comment.create(post_id: subject, author: test_user, text: 'Hola Tom!') } }
+  before { author1.save }
+  before { subject.save }
 
-    it '#five_recent_comments should return 5 comments' do
-      expect(subject.five_recent_comments.size).to eql(subject.comments.last(5).size)
-    end
+  it 'title should be present' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'likes counter should be an integer' do
+    subject.likes_counter = 'a'
+    expect(subject).to_not be_valid
   end
 end
