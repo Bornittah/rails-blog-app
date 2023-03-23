fa465af7f89d0376e9974b402ef0106c2b649ba1
pius ssekweyama <ssekweyamapius@gmail.com>
2023-03-23 15:41:05 +0300

    split controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,43 +1,32 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id].to_i)
-    # @posts = Post.where(author_id: params[:user_id])
-    @post = @user.posts.includes(:comments)
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
-    @post = Post.find_by(author_id: params[:user_id], id: params[:id]) || 'No posts yet'
-    @comments = @post.comments
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+    @comments = @post.comments.includes(:comments)
   end
 
   def new
     @post = Post.new
-    respond_to do |format|
-      format.html { render :new, locals: { post: @post } }
-    end
-  end
-
-  def post_params
-    params
-      .require(:post)
-      .permit(:title, :text)
-      .merge(author: current_user, comments_counter: 0, likes_counter: 0)
   end
 
   def create
     @post = Post.new(post_params)
-    respond_to do |format|
-      format.html do
-        if @post.save
-          flash[:success] = 'Post saved successfully'
-          redirect_to user_post_path(current_user, @post)
-        else
-          flash.now[:error] = 'Error: Post could not be saved'
-          redirect_to new_user_post_path(current_user)
-        end
-      end
+    @post.author_id = current_user.id
+    if @post.save
+      redirect_to "/users/#{current_user.id}/posts"
+    else
+      render :new
     end
   end
 
-  private :post_params
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
