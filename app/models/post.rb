1f90b9a12ddb0d451ff2f29f7b0d3812ae029662
pius ssekweyama <ssekweyamapius@gmail.com>
2023-03-23 15:41:41 +0300

    modals

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,17 +2,21 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: :User, foreign_key: :author_id
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
-  after_save :update_posts_counter
 
-  validates :title, presence: true, length: { minimum: 5, maximum: 250 }
-  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true, length: { maximum: 200 }
+  validates :body, presence: true, length: { minimum: 5 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def recent_comments
+  def most_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
-  def update_posts_counter
+  after_save :update_post_counter
+
+  private
+
+  def update_post_counter
     author.increment!(:posts_counter)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 2, maximum: 30 }
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def recent_posts
-    posts.limit(3).order(created_at: :desc)
+  def most_recent_posts
+    posts.order(created_at: :desc).limit(3)
   end
 end
